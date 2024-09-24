95fa5f90630f3cbb2f259431d6aea196c9dc0ae5
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-09-24 20:11:35 +0100

    Done exercise 3a

---
+++ week-3/3a.py
@@ -0,0 +1,30 @@
+import networkx   as nx
+from sys          import maxsize as infinite
+from Queue import *
+
+def bfs_path_length(graph, first_node):
+    """
+    Compute the shortest path length of the non-directed graph G
+    starting from node first_node. Return a dictionary with the
+    distance (in number of steps) from first_node to all the nodes.
+    """
+
+    distance = {}                 # Diccionario con la distancia desde 
+                                  # firstNode al resto de los nodos.
+    for node in graph.nodes():
+        distance[node] = infinite
+
+    # solve it here!
+    # ...
+    Q = Queue()
+    Q.enqueue(first_node)
+    distance[first_node] = 0
+    
+    while not Q.isEmpty():
+        node = Q.dequeue()
+        for n in graph.neighbors(node):
+            if n not in distance.keys():
+                distance[n] = distance[node] + 1
+                Q.enqueue(n)
+
+    return distance
