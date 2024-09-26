dee32f1591172f205bc9d8c98c0b911273530a27
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-09-26 11:18:27 +0100

    Add minor refactors to improve readability

--- week-3/3b.py
+++ week-3/3b.py
@@ -1,9 +1,14 @@
-import networkx as nx
 directions = {
-    "N": {"L": {"sign": -1, "direction": "W"}, "R": {"sign": 1, "direction": "E"}},
-    "S": {"L": {"sign": 1, "direction": "E"}, "R": {"sign": -1, "direction": "W"}},
-    "E": {"L": {"sign": 1, "direction": "N"}, "R": {"sign": -1, "direction": "S"}},
-    "W": {"L": {"sign": -1, "direction": "S"}, "R": {"sign": 1, "direction": "N"}}
+    "N": {"L": "W", "R": "E"},
+    "S": {"L": "E", "R": "W"},
+    "E": {"L": "N", "R": "S"},
+    "W": {"L": "S", "R": "N"}
+}
+signs = {
+    "N": 1,
+    "S": -1,
+    "E": 1,
+    "W": -1
 }
 def solve(input_list):
     # Resuelve aqui este ejercicio
@@ -12,14 +17,15 @@ def solve(input_list):
     final_coordinates = [0,0]
     # Primer paso será siempre en horizontal.
     direction = "N"
+    coordinate_index = 0
     # El resto de pasos estarán condicionados tanto en eje como en sentido según los pasos anteriores.
     for instruction in input_list:
-        coordinate_index = 1 if is_vertical else 0
-        temp = directions[direction][instruction[0]]
-        sign, direction = temp["sign"], temp["direction"]
+        coordinate_index = coordinate_index % 1
         mov_length = int(instruction[1:])
+        direction = directions[direction][instruction[0]]
+        sign = signs[direction]
         final_coordinates[coordinate_index] += sign * mov_length
-        is_vertical = not is_vertical
+        coordinate_index += 1
     return sum([abs(x) for x in final_coordinates])
 
 first_line = input().split()
