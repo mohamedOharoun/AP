38743a0310e173ed5eb50331afa53d34a0732713
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-10-14 12:20:52 +0100

    Cleaning

--- exs/caminos mínimos/solve.py
+++ exs/caminos mínimos/solve.py
@@ -8,12 +8,8 @@ def solve(graph: nx.DiGraph, u, v):
     visited = set()
     def dfs(n, dist):
         nonlocal dist_max
-        if dist > dist_max:
-            current_path.pop()
-            return
         visited.add(n)
         current_path.append(n)
-        print(current_path, "dfs")
         if current_path[-1] == v:
             if dist < dist_max:
                 solutions_list.clear()
@@ -25,17 +21,15 @@ def solve(graph: nx.DiGraph, u, v):
                 temp = dist + graph.get_edge_data(n, node).get("weight")
                 if temp <= dist_max:
                     dfs(node, temp)
+        visited.remove(n)
         current_path.pop()
-        visited.discard(n)
-        print(current_path, "dfs2")
         return
     current_path.append(u)
     visited.add(u)
     for node in graph.successors(u):
-        print(current_path, "1")
         if node not in visited:
             dist = graph.get_edge_data(u, node)["weight"]
             dfs(node, dist)
         visited.discard(node)
-        print(current_path, "fin")
+        current_path.pop()
     return solutions_list
