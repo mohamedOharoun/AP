2be2576fa3e9d4fc0c1d3aa52c213324850489bc
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-10-16 22:45:16 +0100

    Add comments

--- exs/monedas_dfs_recursivo/solve.py
+++ exs/monedas_dfs_recursivo/solve.py
@@ -6,21 +6,27 @@ def solve(coins, change):
     solutions = []
     def dfs(coin, pos, curr_change):
         nonlocal max_num
+        # Se actualiza el estado en el salto presente.
         curren_path.append(pos)
         current_coins.append(coin)
         curr_change += coin
+        # Se realiza la comprobación de viabilidad.
         if curr_change > change or len(curren_path) > max_num or coin in current_coins[pos-1:]:
             return
+        # Se encuentra solución.
         if curr_change == change:
+            # Si encuentra una solución más óptima se descartan las anteriores y se actualiza el óptimo
             if len(curren_path) < max_num:
                 max_num = len(curren_path)
                 solutions.clear()
             solutions.append(curren_path.copy())
             return
+        # Se avanza en la búsqueda.
         for i, x in enumerate(coins[pos:]):
             dfs(x, pos+i+1, curr_change)
             curren_path.pop()
             current_coins.pop()
+    # Inicializar el camnino con cada moneda.
     for i, x in enumerate(coins):
         dfs(x, i+1, 0)
         curren_path.pop()
