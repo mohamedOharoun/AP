1d83b4ee32df45392ff79852c1b8dd57cdd8cf9f
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-10-17 10:14:45 +0100

    Add comments

--- exs/grafos_disjuntos/solve.py
+++ exs/grafos_disjuntos/solve.py
@@ -11,15 +11,16 @@ def solve(graph: nx.Graph):
                 visited.add(x)
                 current_path.append(x)
                 dfs(x)
+        # Una vez se procesen todos los nodos de un grafo se retorna
         return
 
     for x in graph.nodes():
         if x not in visited:
             visited.add(x)
             current_path.append(x)
-            dfs(x)
+            dfs(x) # Se empiezan a procesar todo el grafo asociado al nodo.
             solutions.append(current_path.copy())
-            current_path.clear()
+            current_path.clear() # Se elimina todos los nodos procesados y se comienza con otro nodo.
     return solutions
 
 first_line = int(input())
