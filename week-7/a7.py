591f88f9dc36863f31a285ea1a222b82e92f4336
Mohamed O. Haroun Zarkik <mohamedhzfp@gmail.com>
2024-10-29 16:40:39 +0000

    Add change
    Improves the implementation to better reflect the value + bound formula and a slimmer solution

--- week-7/a7.py
+++ week-7/a7.py
@@ -11,41 +11,30 @@ class Node:
         return
 
     def estimate(self, items):
-        if self.room < 0:
-            return 0
-    
-        incumbent = 0
-
-        for item in items:
-            incumbent += item.value
-
-        for i in range(self.index):
-            if i not in self.taken:
-                incumbent -= items[i].value
-
-        return incumbent
+        return self.value + sum([item.value for item in items[self.index:]])
 
 
-first_line = input().split()
-item_count = int(first_line[0])
-capacity = int(first_line[1])
-    
-items = []
-for i in range(1, item_count+1):
-    line = input()
-    parts = line.split()
-    items.append(Item(i-1, int(parts[0]), int(parts[1])))
-    
-node1 = Node(1, [], 0, capacity)
-node2 = Node(2, [1], items[1].value, capacity - items[1].weight)
-node3 = Node(3, [2], items[2].value, capacity - items[2].weight)
+if __name__ == "__main__":
+    first_line = input().split()
+    item_count = int(first_line[0])
+    capacity = int(first_line[1])
+        
+    items = []
+    for i in range(1, item_count+1):
+        line = input()
+        parts = line.split()
+        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+        
+    node1 = Node(1, [], 0, capacity)
+    node2 = Node(2, [1], items[1].value, capacity - items[1].weight)
+    node3 = Node(3, [2], items[2].value, capacity - items[2].weight)
 
-print("Node 1: " + str(node1.index) + " " +
-         str(node1.room) + " " + str(node1.taken) + " " +
-         str(node1.value) + " " + str(node1.estimate(items)))
-print("Node 2: " + str(node2.index) + " " +
-         str(node2.room) + " " + str(node2.taken) + " " +
-         str(node2.value) + " " + str(node2.estimate(items)))
-print("Node 3: " + str(node3.index) + " " + 
-         str(node3.room) +" " + str(node3.taken) + " " +
-         str(node3.value) + " " + str(node3.estimate(items)))
+    print("Node 1: " + str(node1.index) + " " +
+            str(node1.room) + " " + str(node1.taken) + " " +
+            str(node1.value) + " " + str(node1.estimate(items)))
+    print("Node 2: " + str(node2.index) + " " +
+            str(node2.room) + " " + str(node2.taken) + " " +
+            str(node2.value) + " " + str(node2.estimate(items)))
+    print("Node 3: " + str(node3.index) + " " + 
+            str(node3.room) +" " + str(node3.taken) + " " +
+            str(node3.value) + " " + str(node3.estimate(items)))
